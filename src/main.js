ed23defa5ebb8db7b645add0bfe5c23f0578b371
csslick <digitone@naver.com>
2025-04-19 15:52:26 +0900

    setup

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="flex items-center justify-center min-h-screen py-2">
+    <h1 class="text-3xl font-bold">Vite App</h1>
+  </div>
+`
+
