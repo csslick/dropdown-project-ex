5f1eb6ec4a43ecc56c116e7a31684580d2ebfb3e
csslick <digitone@naver.com>
2025-04-20 11:27:04 +0900

    module

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -2,7 +2,7 @@ const Dropdown = (element, title, options) => {
   const dropdownHtml = `
     <div class="dropdown relative flex w-[250px] flex-col bg-amber-200">
       <!-- 드롭다운을 열고 닫는 버튼 -->
-      <button class="dropdown-toggle flex justify-between rounded-lg bg-linear-to-r from-[#B13EFB] to-[#FF188C] px-4 py-[10px] font-bold text-white hover:cursor-pointer hover:opacity-80" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false">
+      <button class="dropdown-toggle flex items-center justify-between rounded-lg bg-linear-to-r from-[#B13EFB] to-[#FF188C] px-4 py-[10px] font-bold text-white hover:cursor-pointer hover:opacity-80" type="button" id="dropdownMenuButton" aria-haspopup="true" aria-expanded="false">
         ${title}
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6l-6-6z" /></svg>
       </button>
