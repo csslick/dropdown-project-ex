15e0ba8009e142021a88484ca1b67ee837f4f1de
csslick <digitone@naver.com>
2025-04-30 16:31:47 +0900

    accordion_setup

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Dropdown from './Dropdown'
 
 document.querySelector('#app').innerHTML = `
   <div class="flex flex-col items-center justify-center min-h-screen py-2">
-    <h1 class="text-3xl font-bold mb-2">Vite App</h1>
+    <h1 class="text-3xl font-bold mb-2">Accordion Menu</h1>
     <nav class='flex'></nav>
   </div>
   
